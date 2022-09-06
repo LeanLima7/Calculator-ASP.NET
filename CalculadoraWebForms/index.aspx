0e0372c711561aec530ed8d04f2aad25aa86b345
Leanderson Lima <leanderson.dias.lima@gmail.com>
2022-09-06 12:12:28 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraWebForms/index.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="CalculadoraWebForms.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body style= "background-color: black" text="white">
+    <h1>Calculator WebForms in DarkMode</h1>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Fist Number"></asp:Label><br />
+        <asp:TextBox  ID="txtNro1" runat="server" Width="301px"></asp:TextBox><br /><br />
+
+        <asp:Label ID="Label2" runat="server" Text="Second Number"></asp:Label><br />
+        <asp:TextBox ID="txtNro2" runat="server" Width="301px"></asp:TextBox><br /><br />
+
+        <asp:Button ID="btSomar" runat="server" Text="Addition" OnClick="btSomar_Click" />
+        <asp:Button ID="btSubtrair" runat="server" Text="Subtraction" OnClick="btSubtrair_Click" />
+        <asp:Button ID="btMultiplicar" runat="server" Text="Multiplication" OnClick="btMultiplicar_Click" />
+        <asp:Button ID="btDividir" runat="server" Text="Division" OnClick="btDividir_Click" />
+        <asp:Button ID="btPotencia" runat="server" Text="Potentiation" OnClick="btPotencia_Click" />
+        <asp:Button ID="btRaizq" runat="server" Text="SquareRoot" OnClick="btRaizq_Click" />
+
+        <br /> <br />
+        <asp:Label ID="lbResultado" runat="server" Text="0"></asp:Label>
+
+
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CalculadoraWebForms/index.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace CalculadoraWebForms
+{
+
+
+    public partial class index
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controle txtNro1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNro1;
+
+        /// <summary>
+        /// Controle Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Controle txtNro2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNro2;
+
+        /// <summary>
+        /// Controle btSomar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btSomar;
+
+        /// <summary>
+        /// Controle btSubtrair.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btSubtrair;
+
+        /// <summary>
+        /// Controle btMultiplicar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btMultiplicar;
+
+        /// <summary>
+        /// Controle btDividir.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btDividir;
+
+        /// <summary>
+        /// Controle btPotencia.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btPotencia;
+
+        /// <summary>
+        /// Controle btRaizq.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btRaizq;
+
+        /// <summary>
+        /// Controle lbResultado.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbResultado;
+    }
+}
